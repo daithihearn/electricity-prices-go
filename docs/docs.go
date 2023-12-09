2249fb4f3b3bf29faceef4130cd48b1f437de0f1
Daithi Hearn <daithi.hearn@gmail.com>
2023-12-09 22:35:53 +0100

    unit tests

--- docs/docs.go
+++ docs/docs.go
@@ -320,7 +320,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "2.1.18",
+	Version:          "2.1.19",
 	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
